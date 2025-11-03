c491b474fadfe40eeaa355261cba6bf2f848beaa
Carlos Alberto <c.alberto.ti14@gmail.com>
2025-11-03 13:43:53 -0300

    BAIXAS

--- midia/aniversario/NOVOS/listar_arquivos.bat
+++ midia/aniversario/NOVOS/listar_arquivos.bat
@@ -6,7 +6,7 @@ set "outputFile=Nomes_dos_Arquivos.txt"
 echo Listando arquivos na pasta atual e excluindo os especificados...
 
 REM List files, exclude directories, and then filter out unwanted files
-dir /b /a-d | findstr /v /i "listar_arquivos.bat Nomes_dos_Arquivos.txt NOTA.txt TEXTO_HTML_IA.txt" > "%outputFile%"
+dir /b /a-d | findstr /v /i "listar_arquivos.bat Nomes_dos_Arquivos.txt NOTA.txt TEXTO_HTML_IA.txt K-POP_TEXTO_HTML_IA-ANIVERSARIO.txt" > "%outputFile%"
 
 echo.
 echo Nomes dos arquivos (excluindo os especificados) salvos em "%outputFile%".
